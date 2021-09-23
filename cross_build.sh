c375b2a827384b2c3624ea68b5a2dc3aa7c01f27
leenchan <leenchan@gmail.com>
2021-09-23 18:59:58 +0800

    test

--- cross_build.sh
+++ cross_build.sh
@@ -190,10 +190,10 @@ make -j$(nproc)
 make install
 
 # libtorrent
-LIBTORRENT_VERSION_MAX=$(echo "${QBITTORRENT_VERSION}" | awk -F'.' '{if ($1<=4 && $2 <=1) {print "1.1.14"}}')
+LIBTORRENT_VERSION_MAX=$(echo "${QBITTORRENT_VERSION}" | awk -F'.' '{if ($1<=4 && $2 <=1) {print "libtorrent-1_1_14"}}')
 [ -z "$LIBTORRENT_VERSION_MAX" ] || LIBTORRENT_VERSION="${LIBTORRENT_VERSION_MAX}"
 LIBTORRENT_DL_URL="https://github.com/arvidn/libtorrent/archive/RC_1_2.tar.gz"
-[ -z "$LIBTORRENT_VERSION" ] || LIBTORRENT_DL_URL="https://github.com/arvidn/libtorrent/releases/download/v${LIBTORRENT_VERSION}/libtorrent-rasterbar-${LIBTORRENT_VERSION}.tar.gz"
+[ -z "$LIBTORRENT_VERSION" ] || LIBTORRENT_DL_URL="https://github.com/arvidn/libtorrent/archive/refs/tags/${LIBTORRENT_VERSION}.tar.gz"
 if [ ! -f "${SELF_DIR}/libtorrent.tar.gz" ]; then
 	wget -c -O "${SELF_DIR}/libtorrent.tar.gz" "${LIBTORRENT_DL_URL}"
 fi
